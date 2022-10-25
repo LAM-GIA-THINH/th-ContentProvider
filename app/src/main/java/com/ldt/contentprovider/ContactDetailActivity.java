622bacd58b8e499f9678560d0be20dd296f65fdf
LAM-GIA-THINH <giathinh961617@gmail.com>
2022-10-25 10:12:29 +0700

    first commit

---
+++ app/src/main/java/com/ldt/contentprovider/ContactDetailActivity.java
@@ -0,0 +1,26 @@
+package com.ldt.contentprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ContactDetailActivity extends AppCompatActivity {
+    TextView tvContactName, tvPhoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_detail);
+        anhXa();
+
+        tvContactName.setText(getIntent().getStringExtra("name").toString());
+        tvPhoneNumber.setText(getIntent().getStringExtra("number").toString());
+
+    }
+
+    private void anhXa() {
+        tvContactName = findViewById(R.id.tvContactName);
+        tvPhoneNumber = findViewById(R.id.tvPhoneNumber);
+    }
+}
